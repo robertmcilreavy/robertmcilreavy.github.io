bb52bcea6becc3825e716bfba5af360e9709e6b5
Robert McIlreavy <robertmcilreavy@gmail.com>
2019-01-26 14:26:16 +0000

    update site

--- js/main.js
+++ js/main.js
@@ -72,13 +72,5 @@
         origin: 'bottom',
         scale: 0.8
     });
-    sr.reveal('.sr-contact-1', {
-        delay: 200,
-        scale: 0
-    });
-    sr.reveal('.sr-contact-2', {
-        delay: 400,
-        scale: 0
-    });
 
 })(jQuery); // End of use strict
